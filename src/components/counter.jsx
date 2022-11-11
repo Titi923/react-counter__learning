e26143cf7f123a082c259f6b9a2aefa1701a73c4
Msuconstru <dumitrupetric24@gmail.com>
2022-11-11 12:30:54 +0000

    created a method for a value to change the style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,4 @@
-// imported whatever is needed, but the react is not mandatory to import because is imported by default since lates react updates. 
+// imported whatever is needed, but the React is not mandatory to import because is imported by default since lates react updates. 
 import React, { Component } from 'react';
 
 class Counter extends Component {
@@ -6,26 +6,32 @@ class Counter extends Component {
   state = {
     count: 0,
   };
-
+  
   // this is the render method
   render() {
     // here you can pass javascript code
-    let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
 
     // this is actually what is shown in the browser, the UI 
     return (
       <div>
-        <span className={classes}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
 
+  // this is a function while pressed "Ctrl+Shift+R" and choosed the extract function option, and automatically made me a function, and positioned it in a perfect place of the code
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
   // this is just another function
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    // used object deconstruction, so... to avoid writing everythime "this.state.count"
+    const { count } = this.state; // is like writing "this.state.count" and storing in a variable with a simpler name
+    return count === 0 ? 'Zero' : count; // you can use ternary operator like this too, in the return statement if you maybe forgot
   }
 }
 
